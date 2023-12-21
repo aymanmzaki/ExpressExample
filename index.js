f69271a46edb311cfdc1b91282ab1fbb91bbf31b
aymanzaki <ayman.m.zaki@gmail.com>
2023-12-21 07:43:50 +0200

    add form and process form data

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 let students = require('./data/studentList.json')
+const path = require('path')
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
 app.get('/',(req,res)=>{
     res.send("Hello World");
@@ -14,12 +16,21 @@ app.delete("/",(req,res)=>{
     console.log("Deleteing");
     res.send("Deleted ");
 });
-
 app.post("/student",(req,res)=>{
     let st = req.body;
     students.push(st);
     console.log(st);
     res.send("Ok");
+});
+app.get('/reg',(req,res)=>{
+    res.sendFile(path.join(__dirname,"index.html"))
+});
+app.post("/reg",(req,res)=>{
+    console.log(JSON.stringify(req.body));
+    let data = req.body;
+    console.log(`Name : ${data.nme}`);
+    console.log(`Name : ${data.eml}`);
+    res.status(200).send("OK");
 });
 
 app.listen(8787)
